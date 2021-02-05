9da136fd583840f05c27233e84570a4f7ffc5c18
kodakoda0921 <y.m1911a1.0921@gmail.com>
2021-02-05 22:27:49 +0900

    [fix]画像を変更時にプレビューを作動させるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
           @user.image_preview.attach(params[:user][:image])
           if @user.errors.any?
             flash[:danger] = @user.errors.full_messages.to_s.gsub(",", "<br>").gsub("[", "").gsub("]", "").gsub('"', "").html_safe
-            ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash), user_id: @user.id })
+            ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash,@user), user_id: @user.id })
           end
           @image_flg = true
         end
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
           @user.back_ground_preview.attach(params[:user][:back_ground])
           if @user.errors.any?
             flash[:danger] = @user.errors.full_messages.to_s.gsub(",", "<br>").gsub("[", "").gsub("]", "").gsub('"', "").html_safe
-            ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash), user_id: @user.id })
+            ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash,@user), user_id: @user.id })
           end
           @back_ground_flg = true
         end
