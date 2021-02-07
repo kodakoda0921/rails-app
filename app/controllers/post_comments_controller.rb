6f7c55b5f1e0210b3227671e21cecd8c1d1298d1
kodakoda0921 <y.m1911a1.0921@gmail.com>
2021-02-07 17:29:47 +0900

    [fix]プロフィール編集のリアルタイム化を行う

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,23 +5,23 @@ class PostCommentsController < ApplicationController
   def create
     if params[:post_comment][:content].empty?
       params[:post_comment][:image].nil? ? flash.now[:info] = "空のデータは送信できません。" : flash.now[:info] = "画像のみの投稿はできません。"
-      ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash, current_user), user_id: current_user.id })
+      ActionCable.server.broadcast("flash_channel", { flash: flash, user_id: current_user.id })
     else
       @post_comment = PostComment.new(post_comment_params)
       @post_comment.user_id = current_user.id
       if @post_comment.save
         flash.now[:success] = "投稿に成功しました！"
         ActionCable.server.broadcast("post_comment_channel", { post_comment: @post_comment.html_template, post_comment_count: @post_comment.html_template_count, micropost_id: @post_comment.micropost_id })
-        ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash, current_user), user_id: current_user.id })
+        ActionCable.server.broadcast("flash_channel", { flash: flash, user_id: current_user.id })
         return
       else
         if @post_comment.errors.any?
-          flash.now[:danger] = @post_comment.errors.full_messages.to_s.gsub(",", "<br>").gsub("[", "").gsub("]", "").gsub('"', "").html_safe
-          ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash, current_user), user_id: current_user.id })
+          flash.now[:danger] = @post_comment.errors.full_messages.to_s.gsub(",", "<br>").gsub("[", "").gsub("]", "").gsub('"', "")
+          ActionCable.server.broadcast("flash_channel", { flash: flash, user_id: current_user.id })
           return
         else
           flash.now[:danger] = "不明なエラー"
-          ActionCable.server.broadcast("flash_channel", { flash: flash_template(flash, current_user), user_id: current_user.id })
+          ActionCable.server.broadcast("flash_channel", { flash: flash, user_id: current_user.id })
           return
         end
       end
--- app/javascript/channels/home_channel.js
+++ app/javascript/channels/home_channel.js
@@ -3,14 +3,15 @@ import consumer from "./consumer"
 $(document).on('turbolinks:load', function () {
 
     // js.erb 内で使用できるように変数を定義しておく
-    window.micropostsContainer = document.getElementById('microposts-container')
+    window.micropostsClassContainer = document.getElementsByClassName('microposts-container')
 
     // 以下のプログラムが他のページで動作しないようにしておく
-    if (micropostsContainer === null) {
+    if (micropostsClassContainer === null) {
         return
     }
     let current_user_id_int = document.getElementById("current_user_id").getAttribute("data-user_id");
     const current_user_id = current_user_id_int.toString()
+    const micropostsContainer = document.getElementById('microposts-container-' + current_user_id);
     consumer.subscriptions.create("HomeChannel", {
         connected() {
             // Called when the subscription is ready for use on the server
@@ -23,10 +24,37 @@ $(document).on('turbolinks:load', function () {
         received(data) {
             // Called when there's incoming data on the websocket for this channel
             if (data["user_id"] == current_user_id) {
-                // サーバー側から受け取ったHTMLを一番最後に加える
-                micropostsContainer.insertAdjacentHTML("afterbegin", data["micropost"])
-                document.getElementById('microposts_form-' + current_user_id).reset();
-                document.getElementById("image-name-" + current_user_id).innerHTML = "";
+                if (data["method"] == "create") {
+                    // サーバー側から受け取ったHTMLを一番最後に加える
+                    micropostsContainer.insertAdjacentHTML("afterbegin", data["micropost"])
+                    document.getElementById('microposts_form-' + data["user_id"]).reset();
+                    // document.getElementById("image-name-" + current_user_id).innerHTML = "";
+                    if (document.getElementById("micropost_no_post_anything-" + data["user_id"]) != null) {
+                        $("#micropost_no_post_anything").remove();
+                    }
+                }
+                if (data["method"] == "destroy") {
+                    let micropost_view = "micropost-" + data["micropost_id"]
+                    let micropost_modal = "modal_destroy-" + data["micropost_id"]
+                    $("#" + micropost_modal).modal('hide');
+                    $("#" + micropost_view).remove();
+                }
+                if (data["method"] == "update") {
+                    let user_name_all = document.getElementsByClassName("user_widget_user_name")
+                    let user_job_all = document.getElementsByClassName("user_widget_user_job")
+                    document.getElementById("user_profile_url").innerHTML = data["profiles"].url
+                    document.getElementById("user_profile_location").innerHTML = data["profiles"].location
+                    document.getElementById("user_profile_skill").innerHTML = data["profiles"].skills
+                    document.getElementById("user_profile_notes").innerHTML = data["profiles"].notes
+                    Array.prototype.forEach.call(user_name_all, function (user_name) {
+                        user_name.innerHTML = data["user"].name
+                    })
+                    Array.prototype.forEach.call(user_job_all, function (user_job) {
+                        user_job.innerHTML = data["profiles"].job
+                    })
+                    document.getElementById('profile_password_field-' + current_user_id).value = "";
+                    document.getElementById('profile_password_confirmation_field-' + current_user_id).value = "";
+                }
             }
         }
     })
