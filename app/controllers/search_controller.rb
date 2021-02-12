2c06cca9582a23508e2161b5cfaf67bcce8f3ad7
kodakoda0921 <y.m1911a1.0921@gmail.com>
2021-02-12 23:00:23 +0900

    [fix]空のメッセージが入力された際にエラーメッセージが出力されるように修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,24 @@
 class SearchController < ApplicationController
   def index
     if logged_in?
-      @button = true
-      if params[:tab]
-        @tab_id = SecureRandom.urlsafe_base64
-        @button = false
+      if params[:value].empty?
+        flash.now[:info] = "検索値を入力して下さい"
+        ActionCable.server.broadcast("flash_channel", { flash: flash, user_id: current_user.id })
+        return
+      else
+        @button = true
+        if params[:tab]
+          @tab_id = SecureRandom.urlsafe_base64
+          @button = false
+        end
+        page_tab_id = params[:tab_id]
+        @search_value = params[:value]
+        @current_user = current_user
+        @micropost_search = Micropost.where('content like ?', "%#{params[:value]}%")
+        @post_comment = PostComment.new
+        ActionCable.server.broadcast("search_channel", { microposts: microposts_html_template(@micropost_search), current_user_id: current_user.id.to_s, tab_id: page_tab_id, value: @search_value, method: "new" })
+        return
       end
-      page_tab_id = params[:tab_id]
-      @search_value = params[:value]
-      @current_user = current_user
-      @micropost_search = Micropost.where('content like ?', "%#{params[:value]}%")
-      @post_comment = PostComment.new
-      ActionCable.server.broadcast("search_channel", { microposts: microposts_html_template(@micropost_search), current_user_id: current_user.id.to_s, tab_id: page_tab_id, value: @search_value, method: "new" })
     else
       redirect_to new_session_path
     end
